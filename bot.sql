609f305be3ed3f7e5c6cf7d1dc664d6396622dd1
Maira Ussenbayeva <a01468802@unet.univie.ac.at>
2020-01-14 01:30:21 +0100

    last commit

--- bot.sql
+++ bot.sql
@@ -22,41 +22,41 @@ CREATE TABLE Reservation (
 
 /* Fake data set */
 
-INSERT INTO Resource(name, description) VALUES("Ping-pong Table 1", "test");
-INSERT INTO Resource(name, description) VALUES("Ping-pong Table 2", "test");
-INSERT INTO Resource(name, description) VALUES("Ping-pong Table 3", "test");
-INSERT INTO Resource(name, description) VALUES("Ping-pong Table 4", "test");
+INSERT INTO Resource(name, description) VALUES("Ping-pong Table 1", "test1");
+INSERT INTO Resource(name, description) VALUES("Ping-pong Table 2", "test2");
+INSERT INTO Resource(name, description) VALUES("Ping-pong Table 3", "test3");
+INSERT INTO Resource(name, description) VALUES("Ping-pong Table 4", "test4");
 
-INSERT INTO Resource(name, description) VALUES("BBQ area 1", "test");
-INSERT INTO Resource(name, description) VALUES("BBQ area 2", "test");
-INSERT INTO Resource(name, description) VALUES("BBQ area 3", "test");
-INSERT INTO Resource(name, description) VALUES("BBQ area 4", "test");
-
-
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 1, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 1, "09:30");
-
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 2, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 2, "09:00");
-
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 3, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 3, "09:00");
+INSERT INTO Resource(name, description) VALUES("BBQ area 1", "test5");
+INSERT INTO Resource(name, description) VALUES("BBQ area 2", "test6");
+INSERT INTO Resource(name, description) VALUES("BBQ area 3", "test7");
+INSERT INTO Resource(name, description) VALUES("BBQ area 4", "test8");
 
 
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 4, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 4, "09:00");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 1, "09:01");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 1, "09:32");
 
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 5, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 5, "09:00");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 2, "09:03");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 2, "09:04");
 
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 6, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 6, "09:00");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 3, "09:05");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 3, "09:06");
 
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 7, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 7, "09:00");
 
-INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 8, "09:00");
-INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 8, "09:00");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 4, "09:07");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 4, "09:08");
+
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 5, "09:09");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 5, "09:10");
+
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 6, "09:11");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 6, "09:12");
+
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 7, "09:13");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 7, "09:14");
+
+INSERT INTO Reservation(date, resourceId, time) VALUES ("15.11.2019", 8, "09:15");
+INSERT INTO Reservation(date, resourceId, time) VALUES ("16.11.2019", 8, "09:16");
 
 /* Data to test function getReservedBy */ 
 INSERT INTO Reservation(date, resourceId, reservedBy) VALUES ("17.11.2019", 8, 5);
